3d9f6df435039f25c5bc51602bd4fe70f29a4ea0
AceXiamo <xiamo@qwq.link>
2023-10-20 11:41:25 +0800

    chore: output

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'axm.moe',
-                port: '',
-            },
-        ],
-    }
-}
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "axm.moe",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
